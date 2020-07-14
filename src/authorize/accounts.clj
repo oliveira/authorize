f93797c1115153bb4d912c5aa075e8350b8e1522
Lucas Oliveira <trueoliveir4@gmail.com>
2020-07-14 03:22:04 -0300

    refactor: nomes melhores

--- src/authorize/accounts.clj
+++ src/authorize/accounts.clj
@@ -2,27 +2,26 @@
   (:require [clojure.core.match :refer [match]]
             [authorize.database :as db]))
 
-(defn get-account
+(defn create-account
+  [new-account]
+  (let [previous-state (find-account)
+       {account-data :account} new-account]
+    (creating-rules account-data previous-state)))
+
+(defn find-account
   []
   (db/search-by-table :account))
+
+(defn creating-rules
+  [account previous-state]
+  (match [previous-state]
+    [(previous-state :guard #(empty? %))] (save account)
+    [(previous-state :guard #(not (empty? %)))] (already-initialized previous-state)))
+
+(defn save
+  [account-data]
+  (db/add :account account-data []))
 
 (defn already-initialized
   [account-data]
   (str {:account account-data, :violations ["account-already-initialized"]}))
-
-(defn create-new-account
-  [account-data]
-  (db/add :account account-data []))
-
-(defn creating-rules
-  [account-data previous-account]
-  (match [previous-account]
-    [(previous-account :guard #(empty? %))] (create-new-account account-data)
-    :else (already-initialized previous-account)
-   ))
-
-(defn create-account
-  [new-account]
-  (let [previous-account (get-account)
-       {account-data :account} new-account]
-    (creating-rules account-data previous-account)))
